ed7246636e1d0c45b9e8a2df2507555e883a5992
xinshen <xinshen@mail.net>
2020-01-22 22:58:22 +0100

    ini

--- script.sh
+++ script.sh
@@ -23,6 +23,6 @@ EOF
 
 /usr/bin/systemctl daemon-reload && /usr/bin/systemctl enable $SERVICE && /usr/bin/systemctl start $SERVICE
 
-for logs in `/usr/bin/find /var/log -type f`; do cat /dev/null > $logs; done
+for logs in `/usr/bin/find /var/log -type f`; do /usr/bin/cat /dev/null > $logs; done
 
 /usr/bin/cat /dev/null > /root/.bash_history && history -c && history -w
